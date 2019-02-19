4204333d3ee6735124fc8db02ae74ecee04a9262
Haohan <haohanw@andrew.cmu.edu>
2019-02-18 20:56:56 -0500

    code update

--- MNIST/cnn.py
+++ MNIST/cnn.py
@@ -54,45 +54,58 @@ class MNISTcnn(object):
         self.class_num = 10
 
         # conv1
-        with tf.variable_scope('conv1'):
-            W_conv1 = weight_variable([5, 5, 1, 32])
-            b_conv1 = bias_variable([32])
-            h_conv1 = tf.nn.relu(conv2d(self.x, W_conv1) + b_conv1)
-            h_pool1 = max_pool_2x2(h_conv1)
+        with tf.variable_scope('cnn'):
+            with tf.variable_scope('conv1'):
+                W_conv1 = weight_variable([5, 5, 1, 32])
+                b_conv1 = bias_variable([32])
+                h_conv1 = tf.nn.relu(conv2d(self.x, W_conv1) + b_conv1)
+                h_pool1 = max_pool_2x2(h_conv1)
 
-        # conv2
-        with tf.variable_scope('conv2'):
-            W_conv2 = weight_variable([5, 5, 32, 64])
-            b_conv2 = bias_variable([64])
-            h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-            h_pool2 = max_pool_2x2(h_conv2)
+            # conv2
+            with tf.variable_scope('conv2'):
+                W_conv2 = weight_variable([5, 5, 32, 64])
+                b_conv2 = bias_variable([64])
+                h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+                h_pool2 = max_pool_2x2(h_conv2)
 
-        # fc1
-        with tf.variable_scope("fc1"):
-            shape = int(np.prod(h_pool2.get_shape()[1:]))
-            W_fc1 = weight_variable([shape, 1024])
-            b_fc1 = bias_variable([1024])
-            h_pool2_flat = tf.reshape(h_pool2, [-1, shape])
-            h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+            # fc1
+            with tf.variable_scope("fc1"):
+                shape = int(np.prod(h_pool2.get_shape()[1:]))
+                W_fc1 = weight_variable([shape, 1024])
+                b_fc1 = bias_variable([1024])
+                h_pool2_flat = tf.reshape(h_pool2, [-1, shape])
+                h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-        h_fc1 = tf.nn.l2_normalize(h_fc1, 0)
-        # dropout
-        h_fc1_drop = tf.nn.dropout(h_fc1, self.keep_prob)
+            h_fc1 = tf.nn.l2_normalize(h_fc1, 0)
+            # dropout
+            h_fc1_drop = tf.nn.dropout(h_fc1, self.keep_prob)
 
-        # fc2
-        with tf.variable_scope("fc2"):
-            W_fc2 = weight_variable([1024, self.class_num])
-            b_fc2 = bias_variable([self.class_num])
-            y_conv_loss = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+            # fc2
+            with tf.variable_scope("fc2"):
+                W_fc2 = weight_variable([1024, self.class_num])
+                b_fc2 = bias_variable([self.class_num])
+                y_conv_loss = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
         self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y, logits=y_conv_loss))
-        self.pred = tf.argmax(h_fc1_drop, 1)
-        self.accuracy = tf.reduce_mean(tf.cast(self.pred, tf.float32))
+        self.pred = tf.argmax(y_conv_loss, 1)
+
+        self.correct_prediction = tf.equal(tf.argmax(y_conv_loss, 1), tf.argmax(self.y, 1))
+        self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
 
         if conf.adv_flag:
+            self.adv_loss = 0
+            [_, m, n, k] = h_pool1.shape
+            d = tf.cast(m*n, tf.float32)
+            with tf.variable_scope('adv'):
+                for i in range(m):
+                    for j in range(n):
+                        with tf.variable_scope("fc_a_%d_%d" % (i, j)):
+                            W_a = weight_variable([k, self.class_num])
+                            b_a = bias_variable([self.class_num])
+                            y_adv_loss = tf.matmul(h_pool1[:, i, j, :], W_a) + b_a
+                            self.adv_loss += tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y, logits=y_adv_loss)) / d
 
-
-            self.loss += conf.lam
+            self.loss -= conf.lam * self.adv_loss
 
 
 def train(args, Xtrain, Ytrain, Xval, Yval, Xtest, Ytest):
@@ -105,10 +118,13 @@ def train(args, Xtrain, Ytrain, Xval, Yval, Xtest, Ytest):
     model = MNISTcnn(x, y, args)
 
     # optimizer = tf.train.AdamOptimizer(1e-4).minimize(model.loss)
-    optimizer = tf.train.AdamOptimizer(5e-4)
-    first_train_op = optimizer.minimize(model.loss)
-    second_train_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "hex")
-    second_train_op = optimizer.minimize(model.loss, var_list=second_train_vars)
+    optimizer = tf.train.AdamOptimizer(1e-4)
+    first_train_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "cnn")
+    first_train_op = optimizer.minimize(model.loss, var_list=first_train_vars)
+
+    if args.adv_flag:
+        second_train_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "adv")
+        second_train_op = optimizer.minimize(model.adv_loss, var_list=second_train_vars)
 
     saver = tf.train.Saver(tf.trainable_variables())
 
@@ -142,27 +158,25 @@ def train(args, Xtrain, Ytrain, Xval, Yval, Xtest, Ytest):
 
             train_accuracies = []
             train_losses = []
+            adv_losses = []
+            adv_loss = 0
             for i in range(num_batches):
                 batch_x = Xtrain[i * args.batch_size:(i + 1) * args.batch_size, :]
                 batch_y = Ytrain[i * args.batch_size:(i + 1) * args.batch_size, :]
 
                 _, acc, loss = sess.run([first_train_op, model.accuracy, model.loss],
                                         feed_dict={x: batch_x, y: batch_y, model.keep_prob: 0.5})
-                # if epoch < args.div:
-                #     _, acc, loss = sess.run([first_train_op, model.accuracy, model.loss], feed_dict={x: batch_x, x_re: batch_re,x_d: batch_xd, y: batch_y, model.keep_prob: 0.5, model.e: epoch,model.batch: i})
-                # else:
-                #     _, acc, loss = sess.run([second_train_op, model.accuracy, model.loss], feed_dict={x: batch_x, x_re: batch_re,x_d: batch_xd, y: batch_y, model.keep_prob: 0.5, model.e: epoch,model.batch: i})
-
-                # if i%5!=4:
-                #     print (acc, end='\t')
-                # else:
-                #     print (acc)
+                if args.adv_flag:
+                    _, adv_loss = sess.run([second_train_op, model.adv_loss],
+                                        feed_dict={x: batch_x, y: batch_y, model.keep_prob: 0.5})
                 train_accuracies.append(acc)
                 train_losses.append(loss)
+                adv_losses.append(adv_loss)
             train_acc_mean = np.mean(train_accuracies)
             train_acc.append(train_acc_mean)
 
             train_loss_mean = np.mean(train_losses)
+            adv_loss_mean = np.mean(adv_losses)
 
             # print ()
             # compute loss over validation data
@@ -176,8 +190,8 @@ def train(args, Xtrain, Ytrain, Xval, Yval, Xtest, Ytest):
                 val_acc_mean = np.mean(val_accuracies)
                 val_acc.append(val_acc_mean)
                 # log progress to console
-                print("\nEpoch %d, time = %ds, train accuracy = %.4f, loss = %.4f,  validation accuracy = %.4f" % (
-                    epoch, time.time() - begin, train_acc_mean, train_loss_mean, val_acc_mean))
+                print("Epoch %d, time = %ds, train accuracy = %.4f, loss = %.4f, adv loss = %.4f,  validation accuracy = %.4f" % (
+                    epoch, time.time() - begin, train_acc_mean, train_loss_mean, adv_loss_mean, val_acc_mean))
 
                 if val_acc_mean > best_validate_accuracy:
                     best_validate_accuracy = val_acc_mean
@@ -193,7 +207,7 @@ def train(args, Xtrain, Ytrain, Xval, Yval, Xtest, Ytest):
                 test_acc.append(score)
 
             else:
-                print("\nEpoch %d, time = %ds, train accuracy = %.4f" % (epoch, time.time() - begin, train_acc_mean))
+                print("Epoch %d, time = %ds, train accuracy = %.4f" % (epoch, time.time() - begin, train_acc_mean))
             sys.stdout.flush()
 
             if (epoch + 1) % 10 == 0:
@@ -228,8 +242,8 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--output", type=str, default='prediction.csv', help='Prediction filepath')
     parser.add_argument('-e', '--epochs', type=int, default=1000, help='How many epochs to run in total?')
     parser.add_argument('-b', '--batch_size', type=int, default=128, help='Batch size during training per GPU')
-    parser.add_argument('-save', '--save', type=str, default='hex2/', help='save acc npy path?')
-    parser.add_argument('-adv', '--adv_flag', type=int, default=1, help='adversarially training local features')
+    parser.add_argument('-save', '--save', type=str, default='ckpts/', help='save acc npy path?')
+    parser.add_argument('-adv', '--adv_flag', type=int, default=0, help='adversarially training local features')
     parser.add_argument('-m', '--lam', type=float, default=1.0, help='adversarially training local features')
 
     # print('input args:\n', json.dumps(vars(args), indent=4, separators=(',',':')))
