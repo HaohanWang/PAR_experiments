971b4486ae3b673751d30ebef52e010d07a60a49
Haohan Wang <haohanw@andrew.cmu.edu>
2019-10-23 17:00:23 -0400

    Delete run.sh

--- PACS/run.sh
+++
@@ -1,7 +0,0 @@
-python alexNet.py -adv 1 -cat art -m 0.01 > art.txt
-
-python alexNet.py -adv 1 -cat cartoon -m 0.01 > cartoon.txt
-
-python alexNet.py -adv 1 -cat photo -m 0.01 > photo.txt
-
-python alexNet.py -adv 1 -cat sketch -m 0.01 > sketch.txt
